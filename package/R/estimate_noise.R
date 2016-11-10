db16769ff392cd419c479e98ae361326ae53b244
David Tyler Severson <david.severson@ludwig.ox.ac.uk>
2016-11-10 14:35:49 -0500

    -updated functions for calculate cluster and score metrics to be more intuitive and to be consistent with description in methods -removed normalisation part of the code and thus eliminated need for the dependency on DEseq2 - DEseq2 importation removed from NAMESPACE

--- package/R/estimate_noise.R
+++ package/R/estimate_noise.R
@@ -4,20 +4,9 @@
 
 #### internal functions##################################################
 ##change to datatable, annotate with actual transcript count and optionally normalize by sequencing depth
-prepare_data<-function(x,y, estimateSizeFactors, normseq){
-    if (normseq==TRUE){
-    sizenorms<-estimateSizeFactorsForMatrix(x)
-    x<-sweep(x,2,sizenorms,"/")
-    }
+prepare_data<-function(x,y){
     x$transcripts<-y[rownames(x),1]
     x
-}
-
-#as above for counts table
-prepare_data_counts<-function(x,z,estimateSizeFactors){
-  sizenorms<-estimateSizeFactorsForMatrix(x)
-  z<-sweep(z,2,sizenorms,"/")
-  z
 }
 
 ##computes linear fit for mean and standard deviation across ERCCs in the dataset
@@ -304,63 +293,89 @@ create_cm<-function(z, names){
 ###x is the consensus matrix
 ###y is a dataframe of cluster identities for various k
 
-calculate_cluster_stability<-function(y,x,z){
-  names(y)<-z
-  error.df<-data.frame(rn="", score=-1,Promiscuity=-1,Stability=-1, size=-1)
-  for (i in unique(y)){
-    in.cluster<-as.character(names(y[y==i]))
-    out.cluster<-as.character(names(y[!(y==i)]))
-    if (length(in.cluster)>1){
-      inside.stability<-(sum(x[in.cluster,in.cluster])-length(in.cluster))/(length(in.cluster)^2-length(in.cluster))
-    } else {
-      inside.stability<-1
-    }
-    promiscuity.matrix<-as.matrix(x[in.cluster,out.cluster])
-    symmetry<-1-(length(in.cluster)/(dim(x)[1]-1))
-    qt<-quantile(promiscuity.matrix,symmetry)
-    promiscuity.matrix[promiscuity.matrix<qt]<-NA
-    outside.promiscuity<-mean(promiscuity.matrix, na.rm = TRUE)
-    score<-inside.stability-outside.promiscuity
-    error.df1<-data.frame(rn=as.character(i),score=score, Promiscuity=outside.promiscuity, Stability=inside.stability, size=length(in.cluster))
-    error.df2<-rbind(error.df, error.df1)
-    error.df<-error.df2
+calculate_cluster_metrics_by_cluster<-function(i,y,x){
+  in.cluster<-as.character(names(y[y==i]))
+  out.cluster<-as.character(names(y[!(y==i)]))
+  if (length(in.cluster)>1){
+    inside.stability<-(sum(x[in.cluster,in.cluster, drop=FALSE])-length(in.cluster))/(length(in.cluster)^2-length(in.cluster))
+  } else {
+    inside.stability<-1
   }
-  error.df[-1,]
+  promiscuity.matrix<-as.matrix(x[in.cluster,out.cluster, drop=FALSE])
+  if (length(in.cluster) >= length(out.cluster)){
+    outside.promiscuity<-mean(promiscuity.matrix)
+  } else {
+    prom.mean.dt<-data.table(data.frame(colMeans(promiscuity.matrix)), keep.rownames = TRUE)
+    colnames(prom.mean.dt)[2]<-"mean.prom"
+    prom.mean.dt<-prom.mean.dt[order(-mean.prom)]
+    prom.mean.dt<-prom.mean.dt[1:length(in.cluster),]
+    outside.promiscuity<-mean(promiscuity.matrix[,as.character(prom.mean.dt$rn), drop=FALSE])
+  }
+  score<-inside.stability-outside.promiscuity
+  metrics.dt<-data.table(data.frame(score=score, Promiscuity=outside.promiscuity, Stability=inside.stability, size=length(in.cluster)))
+  metrics.dt
 }
 
-
-
-calculate_cell_stability<-function(y,x,z){
+calculate_cluster_metrics<-function(y,x,z){
   names(y)<-z
-  error.df<-data.frame(rn="",cell="", score=-1,Promiscuity=-1,Stability=-1, cluster.size=-1)
-  for (i in unique(y)){
-    in.cluster<-as.character(names(y[y==i]))
-    out.cluster<-as.character(names(y[!(y==i)]))
-    for (j in unique(in.cluster)){
-      if (length(in.cluster)==1){
-        inside.stability<-1
-        outside.promiscuity<-max(x[j, out.cluster])
-        score<-1-outside.promiscuity
-      } else if (length(in.cluster)==(length(in.cluster)+length(out.cluster))) {
-        inside.stability<-(sum(x[j,in.cluster])-1)/(length(in.cluster)-1)
-        outside.promiscuity<-0
-        score<-inside.stability
-      } else {
-        inside.stability<-(sum(x[j,in.cluster])-1)/(length(in.cluster)-1)
-        promiscuity.matrix<-as.matrix(x[j,out.cluster])
-        symmetry<-1-(length(in.cluster)/(dim(x)[1]-1))
-        qt<-quantile(promiscuity.matrix,symmetry)
-        promiscuity.matrix[promiscuity.matrix<qt]<-NA
-        outside.promiscuity<-mean(promiscuity.matrix, na.rm = TRUE)
-        outside.promiscuity<-(sum(x[j, out.cluster])/length(out.cluster))
-        score<-inside.stability-outside.promiscuity
-      }
-      error.df1<-data.frame(rn=as.character(i),cell=as.character(j),score=score, Promiscuity=outside.promiscuity, Stability=inside.stability, cluster.size=length(in.cluster))
-      error.df2<-rbind(error.df, error.df1)
-      error.df<-error.df2
+  cluster_names<-as.vector(as.character(unique(y)))
+  names(cluster_names)<-as.character(unique(y))
+  metric_list<-lapply(cluster_names, `calculate_cluster_metrics_by_cluster`, x=x, y=y)
+  metric_list<-melt(metric_list, id.vars=c("size"))
+  colnames(metric_list)[c(2,4)]<-c("metric","rn")
+  metric_list
+}
+
+calculate_cell_metrics_by_cluster<-function(i, y, x){
+  in.cluster<-as.character(names(y[y==i]))
+  out.cluster<-as.character(names(y[!(y==i)]))
+  if (length(in.cluster) < length(out.cluster)){
+    if (length(in.cluster)==1){
+      inside.stability<-1
+      outside.promiscuity<-max(x[in.cluster, out.cluster])
+      score<-1-outside.promiscuity
+      cluster_metric.dt<-data.frame(cell=as.character(in.cluster[1]),score=score, Promiscuity=outside.promiscuity, Stability=inside.stability, cluster.size=length(in.cluster))
+      cluster_metric.dt$cluster.size<-length(in.cluster)
+    } else {
+      stability.dt<-data.table(data.frame(x[in.cluster,in.cluster, drop=FALSE]), keep.rownames = TRUE)
+      stability.dt<-stability.dt[,.(Stability = (rowSums(.SD)-1)/(length(in.cluster)-1)), keyby=rn]
+      promiscuity.dt<-data.table(data.frame(x[in.cluster,out.cluster, drop=FALSE]), keep.rownames = TRUE)
+      promiscuity.dt<-promiscuity.dt[,.(Promiscuity = apply(.SD,1, function(x, y=length(in.cluster)) {mean(sort(x, decreasing = TRUE)[1:y])})), keyby=rn]
+      cluster_metric.dt<-stability.dt[promiscuity.dt]
+      colnames(cluster_metric.dt)[1]<-"cell"
+      cluster_metric.dt$score<-cluster_metric.dt$Stability-cluster_metric.dt$Promiscuity
+      cluster_metric.dt$cluster.size<-length(in.cluster)
+    }
+  } else if (length(in.cluster) >= length(out.cluster)) {
+    if (length(in.cluster)==dim(x)[1]) {
+      stability.dt<-data.table(data.frame(x[in.cluster,in.cluster, drop=FALSE]), keep.rownames = TRUE)
+      stability.dt<-stability.dt[,.(Stability = (rowSums(.SD)-1)/(length(in.cluster)-1)), by=rn]
+      stability.dt$Promiscuity<-0
+      colnames(stability.dt)[1]<-"cell"
+      cluster_metric.dt<-stability.dt
+      cluster_metric.dt$cluster.size<-length(in.cluster)
+      cluster_metric.dt$score<-cluster_metric.dt$Stability-cluster_metric.dt$Promiscuity
+    } else {
+      stability.dt<-data.table(data.frame(x[in.cluster,in.cluster,drop=FALSE]), keep.rownames = TRUE)
+      stability.dt<-stability.dt[,.(Stability = (rowSums(.SD)-1)/(length(in.cluster)-1)), keyby=rn]
+      promiscuity.dt<-data.table(data.frame(x[in.cluster,out.cluster, drop=FALSE]), keep.rownames = TRUE)
+      promiscuity.dt<-promiscuity.dt[,.(Promiscuity = rowMeans(.SD)), keyby=rn]
+      cluster_metric.dt<-stability.dt[promiscuity.dt]
+      cluster_metric.dt$score<-cluster_metric.dt$Stability-cluster_metric.dt$Promiscuity
+      cluster_metric.dt$cluster.size<-length(in.cluster)
+      colnames(cluster_metric.dt)<-c("cell", "Stability", "Promiscuity", "score", "cluster.size")
     }
   }
-  error.df[-1,]
+  cluster_metric.dt
+}
+
+calculate_cell_metrics<-function(y,x,z){
+  names(y)<-z
+  cluster_names<-as.vector(as.character(unique(y)))
+  names(cluster_names)<-as.character(unique(y))
+  cell_metric_list<-data.table(melt(lapply(cluster_names, `calculate_cell_metrics_by_cluster`, x=x, y=y), id.vars=c("cell", "cluster.size")))
+  colnames(cell_metric_list)[c(3,5)]<-c("metric","rn")
+  cell_metric_list
 }
 
 ################################################################################
@@ -372,14 +387,8 @@ estimate_noiseparameters<-function(x,y,z, plot=FALSE, sd_inflate=0, normseq=TRUE
   force(granularity)
   force(sd_inflate)
   print("Preparing data for parameter estimation.")
-  ERCC.prepared.counts<-prepare_data(x, y, estimateSizeFactors, normseq=normseq)
-  if (normseq==TRUE){
-    print("Normalizing counts by library size.")
-    actual.prepared.counts<-prepare_data_counts(x,z, estimateSizeFactors)
-  } else {
-    print("Counts will not be normalized as specified by normseq flag.")
-    actual.prepared.counts<-z
-  }
+  ERCC.prepared.counts<-prepare_data(x, y)
+  actual.prepared.counts<-z
   print("Fitting the linear model as a function of gene expression from ERCCs.")
   parameters<-compute_alpha(ERCC.prepared.counts,estimate_mu2sigma, granularity=granularity, sd_inflate=sd_inflate, plot=plot, file=file)
   if (plot==TRUE){
@@ -466,8 +475,8 @@ visualize_model_error<-function(x,y,granularity=300,parameters){
 
 ##seperately plot observed spread of ERCCs, could be useful to compare normalized and unnormalized dispersion
 ##needs editing...
-plot_obs_spread<-function(x,y, file="Rplot",normseq=FALSE,parameters){
-  ERCC.prepared.counts<-prepare_data(x, y, estimateSizeFactors, normseq=normseq)
+plot_obs_spread<-function(x,y, file="Rplot",parameters){
+  ERCC.prepared.counts<-prepare_data(x, y)
   ERCC.m.counts<-melt(data.table(ERCC.prepared.counts, keep.rownames = TRUE), id.vars = c("rn", "transcripts"))
   ERCC.m.valid<-ERCC.m.counts[,mean(value)>0, by="rn"]
   setkey(ERCC.m.valid, "rn")
@@ -555,58 +564,50 @@ report_cell_metrics<-function(y,x){
   y$X1<-rep("1",dim(y)[1])
   y<-data.table(y)
   cell.scores<-list()
-  cell.scores<-lapply(y, `calculate_cell_stability`,x=x, z=z)
-  cell.scores.m<-data.table(melt(cell.scores, id.var=c("rn","cell","cluster.size")))
-  cell.scores.m$L1<-as.factor(gsub("^X","", cell.scores.m$L1))
-  cell.scores.m
+  cell.scores<-lapply(y, `calculate_cell_metrics`,x=x, z=z)
+  cell.scores<-data.table(melt(cell.scores, id.var=c("rn","cell","cluster.size", "metric")))
+  cell.scores$L1<-as.factor(gsub("^X","", cell.scores$L1))
+  cell.scores<-cell.scores[,-5, with=FALSE]
+  cell.scores
 }
 
 ###x is the consensus matrix with rows and columns as cell names
-###y is a dataframe of cluster identities for various k in columns and rownames as cells
-report_cluster_metrics<-function(y,x, plot=FALSE, file="Rplot"){
+###y is a dataframe of cluster identities for various k in columns and rownames as cells  
+report_cluster_metrics<-function(y,x, weighted_mean=FALSE,plot=FALSE, file="Rplot"){
+  print(weighted_mean)
   z<-rownames(y)
   y<-data.table(y)
   cluster.scores<-list()
   oneclust.stability<-(sum(x)-dim(x)[1])/dim(x)[1]^2
-  cluster.scores$X1<-data.frame(rn="1", score=oneclust.stability,Promiscuity=0,Stability=oneclust.stability, size=dim(x)[1])
-  cluster.scores<-append(cluster.scores, lapply(y, `calculate_cluster_stability`,x=x, z=z))
-  cluster.scores.m<-data.table(melt(cluster.scores, id.var=c("rn","size")))
+  cluster.scores$X1<-melt(data.table(rn="1",score=oneclust.stability,Promiscuity=0,Stability=oneclust.stability, size=dim(x)[1]), id.vars=c("rn", "size"))
+  colnames(cluster.scores$X1)[3]<-"metric"
+  cluster.scores<-append(cluster.scores,lapply(y, `calculate_cluster_metrics`,x=x, z=z))
+  cluster.scores.m<-data.table(melt(cluster.scores, id.var=c("rn","size","metric")))
+  cluster.scores.m<-cluster.scores.m[,-4, with=FALSE]
   cluster.scores.m$L1<-gsub("^X","", cluster.scores.m$L1)
   cluster.scores.m$singlet<-cluster.scores.m$size==1
   cluster.scores.m$singlet<-factor(gsub(FALSE,"Cell number > 1", gsub(TRUE, "Cell number = 1", cluster.scores.m$singlet)),levels = c("Cell number > 1","Cell number = 1"))
-  mean.values<-cluster.scores.m[,mean(value), by=c("singlet", "L1", "variable")]
-  setkeyv(mean.values, c("singlet", "L1", "variable"))
-  colnames(mean.values)[4]<-"Mean.singlets"
-  setkeyv(cluster.scores.m, c("singlet", "L1", "variable"))
-  cluster.scores.m<-cluster.scores.m[mean.values]
-  mean.nosinglet.values<-mean.values[singlet!="Cell number = 1",]
-  setkeyv(cluster.scores.m,c("L1", "variable"))
-  setkeyv(mean.nosinglet.values, c("L1", "variable"))
-  colnames(mean.nosinglet.values)[4]<-"Overall.mean"
-  cluster.scores.m<-cluster.scores.m[mean.nosinglet.values[,-1,with=FALSE]]
+  if (weighted_mean==TRUE){
+    mean.overall<-cluster.scores.m[,sum(value*size/as.numeric(cluster.scores.m[L1=="1",]$size[1])), by=c("L1", "metric")]
+  }else {
+    mean.overall<-cluster.scores.m[,mean(value), by=c("L1", "metric")]
+  }
+  colnames(mean.overall)[3]<-"Overall.mean"
+  setkeyv(cluster.scores.m,c("L1", "metric"))
+  setkeyv(mean.overall, c("L1", "metric"))
+  cluster.scores.m<-cluster.scores.m[mean.overall]
   numbers<-as.character(sort(unique(na.omit(as.numeric(gsub("^X","",cluster.scores.m$L1))))))
   other<-unique(cluster.scores.m$L1[!(gsub("X","",cluster.scores.m$L1)%in%numbers)])
-  print(other)
   level<-as.character(c(numbers,other))
-  print(level)
-  print(cluster.scores.m$L1)
   cluster.scores.m$L1<-factor(cluster.scores.m$L1, levels = level)
-  print(cluster.scores.m$L1)
   if (plot==TRUE){
-    g<-ggplot(data=cluster.scores.m, aes(x=L1, y=value))+geom_boxplot(aes(alpha=singlet),color="grey20", fill="grey65")+
-      guides(fill="none", color="none")+xlab("\nCluster number")+ylab("Cluster metric\n")+facet_wrap(~variable, ncol=1,nrow=3, scales = "free")+
-      ggtitle("Cluster technical consensus scores for various clustering\n")+scale_alpha_manual(values=c(1,0.1))+labs(alpha="Cluster size")+
-      geom_point(aes(x=factor(L1), y=Mean.singlets, alpha=singlet),fill="red", size=2.5, color="black", pch=23)+annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf)+
-      annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf)+theme(axis.text.x = element_text(angle = 90, hjust = 1))
-    savefile<-paste(file, "_cluster_singlets_seperated_scores.pdf", sep="")
-    ggsave(savefile, plot=g, device="pdf", width=8.5, height=11, units = "in")
     g<-ggplot(data=cluster.scores.m, aes(x=L1, y=value))+geom_boxplot(color="grey20", fill="grey85")+
-      guides(fill="none", color="none")+xlab("\nCluster number")+ylab("Cluster metric\n")+facet_wrap(~variable, ncol=1,nrow=3, scales = "free")+
+      guides(fill="none", color="none")+xlab("\nCluster number")+ylab("Cluster metric\n")+facet_wrap(~metric, ncol=1,nrow=3, scales = "free")+
       ggtitle("Cluster technical consensus scores for various clustering\n")+labs(alpha="Cluster size")+
       geom_point(aes(x=factor(L1), y=Overall.mean),fill="red", size=2.5, color="black", pch=23)+annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf)+
       annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf)+theme(axis.text.x = element_text(angle = 90, hjust = 1))
     savefile<-paste(file, "_cluster_scores.pdf", sep="")
-    ggsave(savefile, plot=g, device="pdf", width=8.5, height=11, units = "in")
+    ggsave(savefile, plot=g, device="pdf", width=8.5, height=11, units = "in") 
   }
   cluster.scores.m
 }

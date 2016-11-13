ff6afbc5d277b4f9bde09a9ebb221a054d7233f9
David Tyler Severson <david.severson@ludwig.ox.ac.uk>
2016-11-12 21:18:47 -0500

    -substituted x,y, and z variable names in functions to be readable for anyone examining the code. - modified output column names to be more interpretable. - fixed bugs that arose form typos do to variable exchange

--- package/R/cluster_evaluation_tools.R
+++ package/R/cluster_evaluation_tools.R
@@ -5,23 +5,23 @@
 ###underlying function calculating cluster stability###############
 
 ##create cell association matrices for compute consensus
-create_cm<-function(z, names){
-  names(z)<-names
-  sapply(z, function(x){sapply(z, function(y){as.numeric(x==y)})})
+create_cm<-function(cluster_labels, names){
+  names(cluster_labels)<-names
+  sapply(cluster_labels, function(x){sapply(cluster_labels, function(y){as.numeric(x==y)})})
 }
 
 ##functions for cluster stability from estimate_noise.R
 ###x is the consensus matrix
 ###y is a dataframe of cluster identities for various k
-calculate_cluster_metrics_by_cluster<-function(i,y,x){
-  in.cluster<-as.character(names(y[y==i]))
-  out.cluster<-as.character(names(y[!(y==i)]))
+calculate_cluster_metrics_by_cluster<-function(clusters,cluster_labels,consensus_matrix){
+  in.cluster<-as.character(names(cluster_labels[cluster_labels==clusters]))
+  out.cluster<-as.character(names(cluster_labels[!(cluster_labels==clusters)]))
   if (length(in.cluster)>1){
-    inside.stability<-(sum(x[in.cluster,in.cluster, drop=FALSE])-length(in.cluster))/(length(in.cluster)^2-length(in.cluster))
+    inside.stability<-(sum(consensus_matrix[in.cluster,in.cluster, drop=FALSE])-length(in.cluster))/(length(in.cluster)^2-length(in.cluster))
   } else {
     inside.stability<-1
   }
-  promiscuity.matrix<-as.matrix(x[in.cluster,out.cluster, drop=FALSE])
+  promiscuity.matrix<-as.matrix(consensus_matrix[in.cluster,out.cluster, drop=FALSE])
   if (length(in.cluster) >= length(out.cluster)){
     outside.promiscuity<-mean(promiscuity.matrix)
   } else {
@@ -36,125 +36,110 @@ calculate_cluster_metrics_by_cluster<-function(i,y,x){
   metrics.dt
 }
 
-calculate_cluster_metrics<-function(y,x,z){
-  names(y)<-z
-  cluster_names<-as.vector(as.character(unique(y)))
-  names(cluster_names)<-as.character(unique(y))
-  metric_list<-lapply(cluster_names, `calculate_cluster_metrics_by_cluster`, x=x, y=y)
+calculate_cluster_metrics<-function(cluster_labels,consensus_matrix,cluster_names){
+  names(cluster_labels)<-cluster_names
+  clusters<-as.vector(as.character(unique(cluster_labels)))
+  names(clusters)<-as.character(unique(cluster_labels))
+  metric_list<-lapply(clusters, `calculate_cluster_metrics_by_cluster`, consensus_matrix=consensus_matrix, cluster_labels=cluster_labels)
   metric_list<-melt(metric_list, id.vars=c("size"))
   colnames(metric_list)[c(2,4)]<-c("metric","rn")
   metric_list
 }
 
-calculate_cell_metrics_by_cluster<-function(i, y, x){
-  in.cluster<-as.character(names(y[y==i]))
-  out.cluster<-as.character(names(y[!(y==i)]))
+calculate_cell_metrics_by_cluster<-function(cluster_names, cluster_labels, consensus_matrix){
+  in.cluster<-as.character(names(cluster_labels[cluster_labels==cluster_names]))
+  out.cluster<-as.character(names(cluster_labels[!(cluster_labels==cluster_names)]))
   if (length(in.cluster) < length(out.cluster)){
     if (length(in.cluster)==1){
       inside.stability<-1
-      outside.promiscuity<-max(x[in.cluster, out.cluster])
+      outside.promiscuity<-max(consensus_matrix[in.cluster, out.cluster])
       score<-1-outside.promiscuity
-      cluster_metric.dt<-data.frame(cell=as.character(in.cluster[1]),score=score, Promiscuity=outside.promiscuity, Stability=inside.stability, cluster.size=length(in.cluster))
-      cluster_metric.dt$cluster.size<-length(in.cluster)
+      cell_metric.dt<-data.frame(cell=as.character(in.cluster[1]),score=score, Promiscuity=outside.promiscuity, Stability=inside.stability, cluster.size=length(in.cluster))
+      cell_metric.dt$cluster.size<-length(in.cluster)
     } else {
-      stability.dt<-data.table(data.frame(x[in.cluster,in.cluster, drop=FALSE]), keep.rownames = TRUE)
+      stability.dt<-data.table(data.frame(consensus_matrix[in.cluster,in.cluster, drop=FALSE]), keep.rownames = TRUE)
       stability.dt<-stability.dt[,.(Stability = (rowSums(.SD)-1)/(length(in.cluster)-1)), keyby=rn]
-      promiscuity.dt<-data.table(data.frame(x[in.cluster,out.cluster, drop=FALSE]), keep.rownames = TRUE)
+      promiscuity.dt<-data.table(data.frame(consensus_matrix[in.cluster,out.cluster, drop=FALSE]), keep.rownames = TRUE)
       promiscuity.dt<-promiscuity.dt[,.(Promiscuity = apply(.SD,1, function(x, y=length(in.cluster)) {mean(sort(x, decreasing = TRUE)[1:y])})), keyby=rn]
-      cluster_metric.dt<-stability.dt[promiscuity.dt]
-      colnames(cluster_metric.dt)[1]<-"cell"
-      cluster_metric.dt$score<-cluster_metric.dt$Stability-cluster_metric.dt$Promiscuity
-      cluster_metric.dt$cluster.size<-length(in.cluster)
+      cell_metric.dt<-stability.dt[promiscuity.dt]
+      colnames(cell_metric.dt)[1]<-"cell"
+      cell_metric.dt$score<-cell_metric.dt$Stability-cell_metric.dt$Promiscuity
+      cell_metric.dt$cluster.size<-length(in.cluster)
     }
   } else if (length(in.cluster) >= length(out.cluster)) {
-    if (length(in.cluster)==dim(x)[1]) {
-      stability.dt<-data.table(data.frame(x[in.cluster,in.cluster, drop=FALSE]), keep.rownames = TRUE)
+    if (length(in.cluster)==dim(consensus_matrix)[1]) {
+      stability.dt<-data.table(data.frame(consensus_matrix[in.cluster,in.cluster, drop=FALSE]), keep.rownames = TRUE)
       stability.dt<-stability.dt[,.(Stability = (rowSums(.SD)-1)/(length(in.cluster)-1)), by=rn]
       stability.dt$Promiscuity<-0
       colnames(stability.dt)[1]<-"cell"
-      cluster_metric.dt<-stability.dt
-      cluster_metric.dt$cluster.size<-length(in.cluster)
-      cluster_metric.dt$score<-cluster_metric.dt$Stability-cluster_metric.dt$Promiscuity
+      cell_metric.dt<-stability.dt
+      cell_metric.dt$cluster.size<-length(in.cluster)
+      cell_metric.dt$score<-cell_metric.dt$Stability-cell_metric.dt$Promiscuity
     } else {
-      stability.dt<-data.table(data.frame(x[in.cluster,in.cluster,drop=FALSE]), keep.rownames = TRUE)
+      stability.dt<-data.table(data.frame(consensus_matrix[in.cluster,in.cluster,drop=FALSE]), keep.rownames = TRUE)
       stability.dt<-stability.dt[,.(Stability = (rowSums(.SD)-1)/(length(in.cluster)-1)), keyby=rn]
-      promiscuity.dt<-data.table(data.frame(x[in.cluster,out.cluster, drop=FALSE]), keep.rownames = TRUE)
+      promiscuity.dt<-data.table(data.frame(consensus_matrix[in.cluster,out.cluster, drop=FALSE]), keep.rownames = TRUE)
       promiscuity.dt<-promiscuity.dt[,.(Promiscuity = rowMeans(.SD)), keyby=rn]
-      cluster_metric.dt<-stability.dt[promiscuity.dt]
-      cluster_metric.dt$score<-cluster_metric.dt$Stability-cluster_metric.dt$Promiscuity
-      cluster_metric.dt$cluster.size<-length(in.cluster)
-      colnames(cluster_metric.dt)<-c("cell", "Stability", "Promiscuity", "score", "cluster.size")
+      cell_metric.dt<-stability.dt[promiscuity.dt]
+      cell_metric.dt$score<-cell_metric.dt$Stability-cell_metric.dt$Promiscuity
+      cell_metric.dt$cluster.size<-length(in.cluster)
+      colnames(cell_metric.dt)<-c("cell", "Stability", "Promiscuity", "score", "cluster.size")
     }
   }
-  cluster_metric.dt
+  cell_metric.dt
 }
 
-calculate_cell_metrics<-function(y,x,z){
-  names(y)<-z
-  cluster_names<-as.vector(as.character(unique(y)))
-  names(cluster_names)<-as.character(unique(y))
-  cell_metric_list<-data.table(melt(lapply(cluster_names, `calculate_cell_metrics_by_cluster`, x=x, y=y), id.vars=c("cell", "cluster.size")))
+calculate_cell_metrics<-function(cluster_labels,consensus_matrix,clusters){
+  names(cluster_labels)<-clusters
+  cluster_names<-as.vector(as.character(unique(cluster_labels)))
+  cluster_names<-as.character(unique(cluster_labels))
+  cell_metric_list<-data.table(melt(lapply(cluster_names, `calculate_cell_metrics_by_cluster`, consensus_matrix=consensus_matrix, cluster_labels=cluster_labels), id.vars=c("cell", "cluster.size")))
   colnames(cell_metric_list)[c(3,5)]<-c("metric","rn")
   cell_metric_list
 }
 
 ###########USER FUNCTIONS##############################
 ##compute consensus from dataframe of perturbaton reclustering labels
-compute_consensus<-function(x, plot=FALSE, file="Rplot", orig.labels=NA, num.clust=5){
-  print("Calculating consensus matrix...")
-  cm<-lapply(x, `create_cm`,names=rownames(x))
-  cm<-Reduce("+",cm)/dim(x)[2]
-  print("Calculating noise consensus clusterings...")
-  rclust<-hclust(as.dist(abs(1-cm)), method="average")
-  cclust<-hclust(as.dist(abs(1-t(cm))), method="average")
-  clusts <- cutree(cclust, k = num.clust)
-  clusts<-clusts[colnames(cm)]
-  nccolors<-c("darkred", "dodgerblue", "blue3", "limegreen", "darkorange4", "darkcyan", "purple", "gold3","#dad1bf", "#5e4400", "#00abff", "#73a3df", "#d6d1c8","#514627", "maroon3", "#87d6ff", "#004eae", "#a4a097", "#efcf72", "#93d6ff", "#304865", "#76726a", "#b99e4e", "#0077c6", "#7da3d6", "#4a463f", "#847035", "#0077cf", "#55749c", "#e0d0ac", "#52461f", "#0f76be", "#84a2cd", "#ddd0b5", "#f2ce5c", "#004a7d", "#0050bf", "#ab9f86", "#ba9e43", "#9dd5ff", "#5d7393", "#a8a08f", "#85702c", "#31a6ff", "#38485e", "#797161", "#544516", "#004a85", "#b6d4ff", "#e6d099", "#f5ce44", "#0078d8", "#0052d0", "#e3d0a3", "#bd9e2b", "#0079e1", "#8ba2c4", "#ae9f7d", "#867020", "#004b8d", "#63738b", "#7c7159", "#8a7000", "#154975", "#3e4756", "#4d4637", "#fbce00", "#a6d5ff", "#bcd3f7", "#ebcf86", "#bf9d00", "#007ae9", "#c2d3ed", "#e9cf8f", "#564500", "#2f76b5", "#92a2bb", "#b39f6a", "#d1d1d1", "#004c95", "#697383", "#b19f74", "#a0a0a0", "#007cff", "#c8d2e4", "#807148", "#727272", "#5ba5f1", "#0054e1", "#7e7150", "#474747", "#4075ad", "#97a1b2", "#4f462f", "#00ddff", "#004c9d", "#9ca1a9", "#edcf7c", "#29d9ff", "#26496d", "#6e727a", "#b79e58", "#50d8ff", "#aed4ff", "#43474e", "#b59e61", "#67d7ff", "#69a4e8", "#cdd2db", "#82703f", "#79d7ff", "#4c74a4")
-  newclusters<-as.character(clusts[colnames(cm)])
-  if (plot==TRUE && is.na(orig.labels)){
-    print("Creating plot without original cluster labels, because none were provided.")
-    aheatmap(cm,treeheight=80, annCol = list("Consensus Clusters"=newclusters),labCol=NA, labRow=" ", Rowv = rclust, Colv=cclust)
-  } else if (plot==TRUE && !(is.na(orig.labels))) {
-    print("Creating plot with original cluster labels, because they were provided")
-    oldclusters<-as.character(orig.labels[colnames(cm),])
-    aheatmap(cm,treeheight=80, annColors =list("Consensus Clusters"=rev(nccolors), "Original Clusters"=nccolors) ,annCol = list("Consensus Clusters"=newclusters, "Original Clusters"=oldclusters), labCol=NA, labRow=" ", Rowv = rclust, Colv=cclust)
-  }
-  clusts<-data.table(data.frame(clusts), keep.rownames = TRUE)[order(clusts)]
-  list(consensus_matrix=cm,clusts=clusts)
+compute_consensus<-function(cluster_labels, plot=FALSE, file="Rplot"){
+  orig.clusts<-cluster_labels[,length(colnames(cluster_labels))]
+  cluster_labels<-cluster_labels[,-length(colnames(cluster_labels))]
+  cm<-lapply(cluster_labels, `create_cm`,names=rownames(cluster_labels))
+  cm<-Reduce("+",cm)/dim(cluster_labels)[2]
+  cm
 }
 
-cluster_consensus<-function(x,y, method="complete"){
-  cclust<-hclust(as.dist(abs(1-t(x))), method=method)
-  clusts<-data.frame(cutree(cclust, k=y))
+cluster_consensus<-function(consensus_matrix,cluster_num, method="complete"){
+  cclust<-hclust(as.dist(abs(1-t(consensus_matrix))), method=method)
+  clusts<-data.frame(cutree(cclust, k=cluster_num))
   clusts
 }
 
 ##seperately plot cell and cluster noise robustness index
 # y is a data frame of the final clustering results perturbed labels
 # x is a data frame of the consensus matrix. need to make robust to bug when names start with numbers...
-report_cell_metrics<-function(y,x){
-  z<-rownames(x)
-  y$X1<-rep("1",dim(y)[1])
-  y<-data.table(y)
+report_cell_metrics<-function(cluster_labels,consensus_matrix){
+  clusters<-rownames(cluster_labels)
+  cluster_labels$X1<-rep("1",dim(cluster_labels)[1])
+  cluster_labels<-data.table(cluster_labels)
   cell.scores<-list()
-  cell.scores<-lapply(y, `calculate_cell_metrics`,x=x, z=z)
+  cell.scores<-lapply(cluster_labels, `calculate_cell_metrics`,consensus_matrix=consensus_matrix, clusters=clusters)
   cell.scores<-data.table(melt(cell.scores, id.var=c("rn","cell","cluster.size", "metric")))
   cell.scores$L1<-as.factor(gsub("^X","", cell.scores$L1))
   cell.scores<-cell.scores[,-5, with=FALSE]
+  colnames(cell.scores)<-c("Cluster identity", "Cell", "Cluster size", "Metric", "Value", "Clustering")
   cell.scores
 }
 
 ###x is the consensus matrix with rows and columns as cell names
 ###y is a dataframe of cluster identities for various k in columns and rownames as cells
-report_cluster_metrics<-function(y,x, weighted_mean=FALSE,plot=FALSE, file="Rplot"){
-  print(weighted_mean)
-  z<-rownames(y)
-  y<-data.table(y)
+report_cluster_metrics<-function(cluster_labels,consensus_matrix, weighted_mean=FALSE,plot=FALSE, file="Rplot"){
+  cluster_names<-rownames(cluster_labels)
+  cluster_labels<-data.table(cluster_labels)
   cluster.scores<-list()
-  oneclust.stability<-(sum(x)-dim(x)[1])/dim(x)[1]^2
-  cluster.scores$X1<-melt(data.table(rn="1",score=oneclust.stability,Promiscuity=0,Stability=oneclust.stability, size=dim(x)[1]), id.vars=c("rn", "size"))
+  oneclust.stability<-(sum(consensus_matrix)-dim(consensus_matrix)[1])/dim(consensus_matrix)[1]^2
+  cluster.scores$X1<-melt(data.table(rn="1",score=oneclust.stability,Promiscuity=0,Stability=oneclust.stability, size=dim(consensus_matrix)[1]), id.vars=c("rn", "size"))
   colnames(cluster.scores$X1)[3]<-"metric"
-  cluster.scores<-append(cluster.scores,lapply(y, `calculate_cluster_metrics`,x=x, z=z))
+  cluster.scores<-append(cluster.scores,lapply(cluster_labels, `calculate_cluster_metrics`,consensus_matrix=consensus_matrix, cluster_names=cluster_names))
   cluster.scores.m<-data.table(melt(cluster.scores, id.var=c("rn","size","metric")))
   cluster.scores.m<-cluster.scores.m[,-4, with=FALSE]
   cluster.scores.m$L1<-gsub("^X","", cluster.scores.m$L1)
@@ -182,5 +167,6 @@ report_cluster_metrics<-function(y,x, weighted_mean=FALSE,plot=FALSE, file="Rplo
     savefile<-paste(file, "_cluster_scores.pdf", sep="")
     ggsave(savefile, plot=g, device="pdf", width=8.5, height=11, units = "in")
   }
+  colnames(cluster.scores.m)<-c("Cluster identity", "Cluster size", "Metric", "Value", "Clustering", "Singlet", "Clustering Mean")
   cluster.scores.m
 }
